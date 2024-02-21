fbeb498d92370ccfc66a76cd5312eec6d3114298
Marat Sisenov <cool.marats@yandex.ru>
2024-02-21 13:03:02 +0300

    1lw fin?

--- lab1.py
+++ lab1.py
@@ -27,6 +27,9 @@ y = r * sp.sin(phi)
 Vx = sp.diff(x, t)
 Vy = sp.diff(y, t)
 V = sp.sqrt(Vx ** 2 + Vy ** 2)
+Ax = sp.diff(Vx, t)
+Ay = sp.diff(Vy, t)
+A = sp.sqrt(Ax ** 2 + Ay ** 2)
 
 
 R = np.zeros_like(T)
@@ -35,6 +38,8 @@ X = np.zeros_like(T)
 Y = np.zeros_like(T)
 VX = np.zeros_like(T)
 VY = np.zeros_like(T)
+AX = np.zeros_like(T)
+AY = np.zeros_like(T)
 
 # заполнение массивов
 for i in np.arange(len(T)):
@@ -44,6 +49,8 @@ for i in np.arange(len(T)):
     Y[i] = sp.Subs(y, t, T[i])
     VX[i] = sp.Subs(Vx, t, T[i])
     VY[i] = sp.Subs(Vy, t, T[i])
+    AX[i] = sp.Subs(Ax, t, T[i])
+    AY[i] = sp.Subs(Ay, t, T[i])
 
 # origin
 XX = [0 for i in range(1000)]
@@ -61,6 +68,9 @@ P, = ax1.plot(X[0], Y[0], 'r', marker='o')
 # рисуем вектор в нулевой момент времени (в anima будем рисовать с 1 до последнего момента времени)
 v_line, = ax1.plot([X[0], X[0] + VX[0]], [Y[0], Y[0] + VY[0]], 'r')
 v_line3, = ax1.plot([XX[0], X[0]], [YY[0], Y[0]], 'b')
+# рисуем вектор ускорения в нулевой момент времени (в anima будем рисовать с 1 до последнего момента времени)
+a_line, = ax1.plot([X[0], X[0] + AX[0]], [Y[0], Y[0] + AY[0]], 'g')
+
 
 
 ArrowX = np.array([-0.2 * Radius, 0, -0.2 * Radius])
@@ -69,24 +79,33 @@ ArrowWX = np.array([-Radius, 0, -Radius])
 ArrowWY = np.array([Radius, 0, -Radius])
 ArrowRX = np.array([-0.1 * Radius, 0, -0.1 * Radius])
 ArrowRY = np.array([0.05 * Radius, 0, -0.05 * Radius])
+# Координаты концов вектора ускорения
+ArrowAX = np.array([-0.2 * Radius, 0, -0.2 * Radius])
+ArrowAY = np.array([0.1 * Radius, 0, -0.1 * Radius])
+
 
 RArrowX, RArrowY = Rot2D(ArrowX, ArrowY, math.atan2(VY[0], VX[0]))
 RArrowRX, RArrowRY = Rot2D(ArrowRX, ArrowRY, math.atan2(X[0], Y[0]))
+# Поворот вектора ускорения
+RArrowAX, RArrowAY = Rot2D(ArrowAX, ArrowAY, math.atan2(AY[0], AX[0]))
 VArrow, = ax1.plot(RArrowX + X[0] + VX[0], RArrowY + Y[0] + VY[0], 'r')
 RArrow, = ax1.plot(ArrowRX + X[0], ArrowRY + Y[0], 'b')
+# Отображение вектора ускорения на графике
+AArrow, = ax1.plot(RArrowAX + X[0] + AX[0], RArrowAY + Y[0] + AY[0], 'g')
 
 
 # рисуем в каждый момент времени i нужные нам вектора
 def anima(j):
     P.set_data(X[j], Y[j])
     v_line.set_data([X[j], X[j] + VX[j]], [Y[j], Y[j] + VY[j]])
+    a_line.set_data([X[j], X[j] + AX[j]], [Y[j], Y[j] + AY[j]])#
     v_line3.set_data([XX[j], X[j]], [YY[j], Y[j]])
     r_arrow_x, r_arrow_y = Rot2D(ArrowX, ArrowY, math.atan2(VY[j], VX[j]))
     VArrow.set_data(r_arrow_x + X[j] + VX[j], r_arrow_y + Y[j] + VY[j])
     r_arrow_rx, r_arrow_ry = Rot2D(ArrowRX, ArrowRY, math.atan2(Y[j], X[j]))
     RArrow.set_data(r_arrow_rx + X[j], r_arrow_ry + Y[j])
 
-    return P, v_line, VArrow, v_line3, RArrow
+    return P, v_line, VArrow, v_line3, RArrow, a_line#
 
 
 anim = FuncAnimation(fig, anima, frames=1000, interval=20, blit=True, repeat=True)
